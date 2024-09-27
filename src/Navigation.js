ea98fa9cb19908f9ff52cb2ccdf992caf86f0198
VivinThambidurai <128116744+VivinThambidurai@users.noreply.github.com>
2024-09-27 14:40:57 +0530

    Update Navigation.js

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,6 @@
 export default function Navigation(){
     return <nav>
-        <h1>Title</h1>
+        <h1>Heyy Buddy This is Vivin Here ...Nice to see You Have a good Day </h1>
         
     </nav>
 }
